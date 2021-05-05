94b606c8a5f8e7e112c82cb11c38fde24aa5e8a2
michael <zdong2010@gmail.com>
2021-05-05 22:53:16 +0800

    更新http-server.js

--- http-server.js
+++ http-server.js
@@ -1,6 +1,9 @@
 const http = require('http')
 
 const app = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
     res.write('hello ~')
     res.end()
 })
